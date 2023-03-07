4d550be64bf3f299c9fe2aafe4accb13316b4e8c
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-03-07 23:27:55 +0900

    format code

--- tests/test_log.py
+++ tests/test_log.py
@@ -5,29 +5,31 @@ from wcpan.logger import setup, INFO
 
 
 class LogTestCase(TestCase):
-
     def test_log(self):
         with NamedTemporaryFile() as fio:
-            setup([
-                'short',
-                'long.long',
-            ], fio.name)
+            setup(
+                [
+                    "short",
+                    "long.long",
+                ],
+                fio.name,
+            )
 
-            INFO('short') << 'test'
-            INFO('long.long') << 'test'
-            INFO('long.long.long') << 'test'
-            INFO('short') << 'test'
+            INFO("short") << "test"
+            INFO("long.long") << "test"
+            INFO("long.long.long") << "test"
+            INFO("short") << "test"
 
             fio.seek(0)
             line = fio.readline()
-            pos = line.find(b'|short|')
+            pos = line.find(b"|short|")
             self.assertGreaterEqual(pos, 0)
             line = fio.readline()
-            pos = line.find(b'|long.long|')
+            pos = line.find(b"|long.long|")
             self.assertGreaterEqual(pos, 0)
             line = fio.readline()
-            pos = line.find(b'|long.long.long|')
+            pos = line.find(b"|long.long.long|")
             self.assertGreaterEqual(pos, 0)
             line = fio.readline()
-            pos = line.find(b'|short_________|')
+            pos = line.find(b"|short_________|")
             self.assertGreaterEqual(pos, 0)
--- wcpan/logger/log.py
+++ wcpan/logger/log.py
@@ -5,62 +5,60 @@ from typing import Any, Iterable, List, Optional
 
 
 class Logger(object):
-
-    def __init__(self, name: str, level: str,
-                 exception: Optional[Exception]) -> None:
+    def __init__(self, name: str, level: str, exception: Optional[Exception]) -> None:
         super().__init__()
         self._logger = logging.getLogger(name)
         self._level = level
         self._parts = []
         self._exception = exception
 
-    def __lshift__(self, part: Any) -> 'Logger':
+    def __lshift__(self, part: Any) -> "Logger":
         self._parts.append(str(part))
         return self
 
     def __del__(self) -> None:
-        msg = ' '.join(self._parts)
+        msg = " ".join(self._parts)
         log = getattr(self._logger, self._level)
         log(msg, exc_info=self._exception)
 
 
 def DEBUG(name: str) -> Logger:
-    return Logger(name, 'debug', None)
+    return Logger(name, "debug", None)
 
 
 def INFO(name: str) -> Logger:
-    return Logger(name, 'info', None)
+    return Logger(name, "info", None)
 
 
 def WARNING(name: str) -> Logger:
-    return Logger(name, 'warning', None)
+    return Logger(name, "warning", None)
 
 
 def ERROR(name: str) -> Logger:
-    return Logger(name, 'error', None)
+    return Logger(name, "error", None)
 
 
 def CRITICAL(name: str) -> Logger:
-    return Logger(name, 'critical', None)
+    return Logger(name, "critical", None)
 
 
 def EXCEPTION(name: str, exception: Exception = None) -> Logger:
-    return Logger(name, 'exception', exception)
+    return Logger(name, "exception", exception)
 
 
-def setup(log_name_list: Iterable[str],
-          file_path: str = None) -> List[logging.Logger]:
+def setup(log_name_list: Iterable[str], file_path: str = None) -> List[logging.Logger]:
     formatter = DynamicFormatter()
     handler = create_handler(file_path, formatter)
     loggers = [create_logger(name, handler) for name in log_name_list]
     return loggers
 
 
-def create_handler(path: Optional[str],
-                   formatter: logging.Formatter) -> logging.Handler:
+def create_handler(
+    path: Optional[str], formatter: logging.Formatter
+) -> logging.Handler:
     if path:
         # rotate on Sunday
-        handler = TimedRotatingFileHandler(path, when='w6', atTime=datetime.time())
+        handler = TimedRotatingFileHandler(path, when="w6", atTime=datetime.time())
     else:
         handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
@@ -77,13 +75,12 @@ def create_logger(name: str, handler: logging.Handler) -> logging.Logger:
 
 
 class DynamicFormatter(object):
-
     FORMAT = (
-        '{{asctime}}|'
-        '{{threadName:_<{thread_name_size}.{thread_name_size}}}|'
-        '{{levelname:_<1.1}}|'
-        '{{name:_<{name_size}.{name_size}}}|'
-        '{{message}}'
+        "{{asctime}}|"
+        "{{threadName:_<{thread_name_size}.{thread_name_size}}}|"
+        "{{levelname:_<1.1}}|"
+        "{{name:_<{name_size}.{name_size}}}|"
+        "{{message}}"
     )
 
     def __init__(self):
@@ -96,7 +93,7 @@ class DynamicFormatter(object):
             name_size=self._name_size,
             thread_name_size=self._thread_name_size,
         )
-        return logging.Formatter(format, style='{')
+        return logging.Formatter(format, style="{")
 
     def format(self, record: logging.LogRecord):
         changed = False
