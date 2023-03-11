a3cd7203cd5e775c5fb2d864e65817ce8db93bc4
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-03-11 10:38:23 +0900

    fix module version

--- wcpan/logging/__init__.py
+++ wcpan/logging/__init__.py
@@ -3,6 +3,5 @@ from importlib.metadata import version
 from .builder import ConfigBuilder
 
 
-version(__package__ or __name__)
-
+__version__ = version(__package__ or __name__)
 __all__ = ("ConfigBuilder",)
